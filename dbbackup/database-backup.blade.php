dadd941f1615f8ef0677ffabb5a22e021ab8eec4
Your NameKumar <brilliant88work@gmail.com>
2019-07-16 18:54:41 +0530

    First Commit

---
+++ dbbackup/database-backup.blade.php
@@ -0,0 +1,4 @@
+Click & Backup
+{!! Form::open(['route'=>'databackup','method'=>'post','role'=>'form','class'=>'form-horizontal']) !!}
+<button type="submit" name="backup" value=" Backup " id="button_backup">Backup</button>
+{!! Form::close() !!}
